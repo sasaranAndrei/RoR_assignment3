47fe042962fb9eb77e3fac37666e2d71e97e9ce1
sasaranAndrei <sasaranandreipaul@gmail.com>
2021-04-29 13:05:40 +0300

    Add all CRUD operations

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,44 @@
+class ProductsController < ApplicationController
+  
+  before_action :current_product, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    Product.create(product_params)
+    redirect_to products_path
+  end
+
+  def edit
+  end
+
+  def update
+    @product.update(product_params)
+    redirect_to products_path
+  end
+
+  def destroy 
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price)
+  end
+
+  def current_product
+    @product = Product.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  get 'product/new'
-  get 'product/show'
-  get 'product/edit'
-  get 'product/delete'
+  get 'products/index'
+  get 'products/show'
+  get 'products/new'
+  get 'products/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'pages/index'
   resources :products
