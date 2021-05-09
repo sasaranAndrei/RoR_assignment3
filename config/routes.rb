4dc3dccc57bb956b928409482a683797e0aa2987
sasaranAndrei <sasaranandreipaul@gmail.com>
2021-05-09 14:30:11 +0300

    Create layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
+  get '/dashboard', to: 'products#index'
   root 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'pages/index'
