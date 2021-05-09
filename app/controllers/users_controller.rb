88e083b282667a8ad4f1afe4a534d36d480728cf
sasaranAndrei <sasaranandreipaul@gmail.com>
2021-05-09 19:56:02 +0300

    Add Show User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
+  
+  before_action :current_user, only: [:show, :edit, :update, :destroy]
+  
+  def show; end
+
   def new
+    @user = User.new
+  end
+
+  private 
+
+  def current_user
+    @user = User.find(params[:id])
   end
 end
