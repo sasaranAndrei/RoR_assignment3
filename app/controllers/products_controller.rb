94cb4781b2089673197d9b714ba9593447444176
sasaranAndrei <sasaranandreipaul@gmail.com>
2021-05-10 15:23:09 +0300

    Add basic Cart Functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  
+  before_action :initialize_session, :load_cart
   before_action :current_product, only: [:show, :edit, :update, :destroy]
   
   def index
@@ -39,7 +39,21 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def add_to_cart 
+    id = params[:id].to_i
+    session[:cart] << id unless session[:cart].include?(id)
+    redirect_to root_path
+  end
+
   private
+
+  def initialize_session
+    session[:cart] ||= []
+  end
+
+  def load_cart
+    @cart = Product.find(session[:cart])
+  end
 
   def product_params
     params.require(:product).permit(:title, :description, :price, :picture)
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/signup', to: 'users#new'
+  post 'products/add_to_cart/:id', to: 'products#add_to_cart', as: 'add_to_cart'
+  delete 'products/remove_from_cart/:id', to: 'products#remove_from_cart', as: 'remove_from_cart'
+  get '/cart', to: 'products#cart', as: 'cart'
   get '/dashboard', to: 'products#index'
   root 'pages#index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'pages/index'
   resources :products
