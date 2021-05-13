f7be210c9cb3cbaa7df6ca8839185ed29b907d80
sasaranAndrei <sasaranandreipaul@gmail.com>
2021-05-13 14:02:35 +0300

    Add Remember Me

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(session_params[:password])
       reset_session
       log_in user
+      session_params[:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash[:danger] = 'Invalid email/password combination'
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
