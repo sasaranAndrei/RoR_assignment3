c9fdeffb2de20aefee4147caabe51e7822225d44
sasaranAndrei <sasaranandreipaul@gmail.com>
2021-05-18 16:48:15 +0300

    Refactor gemfile

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,6 @@ class Product < ApplicationRecord
   validates :description, presence: true, inclusion: { in: %W(#{ "Main courses" } #{ "Second courses" } Soups Entrees Salads Desserts) }
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :picture, presence: true
+  
   has_one_attached :picture
 end
