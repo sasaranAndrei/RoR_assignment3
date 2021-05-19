6f66baa06314eb64922d689e65588841fd3fd407
sasaranAndrei <sasaranandreipaul@gmail.com>
2021-05-19 10:31:49 +0300

    Set dev active_storage to Cloudinary

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   config.action_mailer.raise_delivery_errors = false
 
