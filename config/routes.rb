bace59c2ff6dd66a0ecd64dbee5b4e6bc9f8d7b5
sasaranAndrei <sasaranandreipaul@gmail.com>
2021-05-20 13:51:32 +0300

    Add Password Reset emails

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  get '/signup', to: 'users#new'  
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get     '/signup',  to: 'users#new'  
+  get     '/login',   to: 'sessions#new'
+  post    '/login',   to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy'
 
-  get '/cart', to: 'products#cart', as: 'cart'
+  get '/cart',      to: 'products#cart',  as: 'cart'
   get '/dashboard', to: 'products#index', as: 'dashboard'
   resources :products do
     post :add_to_cart, on: :member
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   resources :products
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: %i[new create edit update]
 end
