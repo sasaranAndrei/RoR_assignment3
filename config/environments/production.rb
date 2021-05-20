4a575acc582a421aff2a46a75053d288e0ae2a27
sasaranAndrei <sasaranandreipaul@gmail.com>
2021-05-20 11:04:02 +0300

    Add mailgun vars to .env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'https://polar-depths-33881.herokuapp.com/'
-  config.action_mailer.default_url_options = { host: host}
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
