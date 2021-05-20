d14d46b9d177558d84d34c85e3aefe77c7e3e1b4
sasaranAndrei <sasaranandreipaul@gmail.com>
2021-05-20 14:07:58 +0300

    Add password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,8 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: %i[edit update]
+  before_action :valid_user, only: %i[edit update]
+  before_action :check_expiration, only: %i[edit update] 
+
   def new
   end
 
@@ -14,5 +18,38 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render('edit')
+    elsif @user.update(user_params)
+      reset_session
+      log_in(@user)
+      redirect_to(@user)
+    else
+      render('edit')
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  def valid_user
+    redirect_to(root_url) unless @user && @user.activated? && @user.authenticated(:reset, params[:id])
+  end
+
+  def check_expiration
+    if @user.password_reset_expired?
+      redirect_to(new_password_reset_url)
+    end
   end
 end
