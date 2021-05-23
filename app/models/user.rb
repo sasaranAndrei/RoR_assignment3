68ce7e9702c662ee84a13f0222037e3bd71d435c
sasaranAndrei <sasaranandreipaul@gmail.com>
2021-05-23 23:16:12 +0300

    Mock users in previews

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
   
-  before_save   :downcase_email!
-  before_create :create_activation_digest!
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, 
@@ -67,11 +67,11 @@ class User < ApplicationRecord
 
   private
 
-  def downcase_email!
+  def downcase_email
     self.email = email.downcase
   end
 
-  def create_activation_digest!
+  def create_activation_digest
     self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,12 +1,12 @@
 class UserMailerPreview < ActionMailer::Preview
   def account_activation
-    user = User.first
+    user = User.new(name: 'Name', email: 'email@domain.com', password: 'password', password_confirmation: 'password')
     user.activation_token = User.new_token
     UserMailer.account_activation(user)
   end
 
   def password_reset
-    user = User.first
+    user = User.new(name: 'Name', email: 'email@domain.com', password: 'password', password_confirmation: 'password')
     user.reset_token = User.new_token
     UserMailer.password_reset(user)
   end
