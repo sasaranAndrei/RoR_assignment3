5f0fff670e585ddfd525da14588a286000232b8c
sasaranAndrei <sasaranandreipaul@gmail.com>
2021-05-28 13:11:20 +0300

    Fix: Time.now + 10.minutes for user to complete the form

--- app/validators/future_validator.rb
+++ app/validators/future_validator.rb
@@ -1,5 +1,5 @@
 class FutureValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors[attribute] << options[:message] || "can't be in the past" if record[attribute] < Time.now
+    record.errors[attribute] << options[:message] || "can't be in the past" if record[attribute] + 10.minutes < Time.now
   end
 end
